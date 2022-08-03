d26a7cb70bc622f01eb15a76c0ff9a8fc0554ac8
PietroLinguini <rayhmiot@gmail.com>
2022-08-03 15:39:03 -0400

    polished up the high score board.

--- src/main/kotlin/components/menu/HighscoreBoard.kt
+++ src/main/kotlin/components/menu/HighscoreBoard.kt
@@ -4,13 +4,11 @@ import csstype.*
 import emotion.react.css
 import react.FC
 import react.Props
-import react.dom.html.ReactHTML
 import react.dom.html.ReactHTML.div
 import react.dom.html.ReactHTML.p
 import react.dom.html.ReactHTML.section
-import react.dom.html.ReactHTML.span
 
-val DUMMY_SCORES = listOf(100, 200, 300, 400)
+val DUMMY_SCORES = listOf<ULong>()
 
 val HighscoreBoard = FC<Props> {
     div {
@@ -24,7 +22,6 @@ val HighscoreBoard = FC<Props> {
             css {
                 fontSize = (2.4).rem
                 marginBottom = (0.2).rem
-                borderBottom = Border((0.8).rem, LineStyle.double, Color("#fff"))
                 width = 40.rem
             }
             +"High Scores:"
@@ -37,41 +34,55 @@ val HighscoreBoard = FC<Props> {
                 fontSize = (2.2).rem
                 marginLeft = (-1.2).rem
                 overflow = Overflow.hidden
+                borderTop = Border((0.8).rem, LineStyle.double, Color("#fff"))
             }
 
             section {
-                css {
-                    marginLeft = (2.6).rem
-                }
-                (1..DUMMY_SCORES.size).forEach {
-                    section {
-                        css { zIndex = integer(2); position = Position.relative }
-                        if (it in 1..3) div {
-                            css {
-                                position = Position.absolute
-                                background =
-                                    "radial-gradient(ellipse closest-side, ${
-                                        when (it) {
-                                            1 -> "hsl(51deg 100% 15%)"
-                                            2 -> "hsl(51deg 0% 21%)"
-                                            3 -> "hsl(39deg 26% 18%)"
-                                            else -> "black"
-                                        }
-                                    }, transparent)".unsafeCast<Background>()
-                                width = 500.pct
-                                height = 100.pct
-                                left = (-107).pct
-                                zIndex = integer(-1)
-                            }
+                css { gridColumn = "1 / 2 / span 1 / span 1".unsafeCast<GridArea>() }
+                if (DUMMY_SCORES.isEmpty()) {
+                    p {
+                        css { color = Color("green"); width = 20.rem}
+                        +"Play some games and get some scores!"
+                    }
+                } else {
+                    css { zIndex = integer(2) }
+                    DUMMY_SCORES.sortedDescending().forEachIndexed { index, score ->
+                        section {
+                            css { position = Position.relative }
+                            if (index in 0..2)
+                                div {
+                                    css {
+                                        position = Position.absolute
+                                        background =
+                                            "radial-gradient(ellipse closest-side, ${
+                                                when (index) {
+                                                    0 -> "hsl(51deg 100% 15%)"
+                                                    1 -> "hsl(51deg 0% 21%)"
+                                                    2 -> "hsl(39deg 26% 18%)"
+                                                    else -> "black"
+                                                }
+                                            }, transparent)".unsafeCast<Background>()
+                                        width = 75.pct
+                                        height = 100.pct
+                                        left = (-25).pct
+                                        zIndex = integer(-1)
+                                    }
+                                }
+                            p { +"$score" }
                         }
-                        p { +"$it." }
                     }
                 }
             }
             section {
-                css { zIndex = integer(2) }
-                DUMMY_SCORES.sortedDescending().forEach {
-                    p { +"$it" }
+                css {
+                    marginLeft = (1.2).rem
+                    gridArea = "1 / 1 / span 1 / span 1".unsafeCast<GridArea>()
+                }
+                (1..5).forEach {
+                    p {
+                        css { zIndex = integer(2); position = Position.relative }
+                        +"$it."
+                    }
                 }
             }
         }
